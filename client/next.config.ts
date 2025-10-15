8ffd312732c27620efc7c255a00929ce58f9b534
Roman Levkiv <romanwdwd@gmail.com>
2025-10-16 02:41:07 +0300

    add minukube logic

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://backend-service:8080/api/:path*",
+      },
+    ];
+  },
   images: {
     domains: ["jawg.io"], // Add any image domains you're using
     formats: ["image/avif", "image/webp"],
